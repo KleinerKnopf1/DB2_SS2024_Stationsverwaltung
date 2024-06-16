c89aa5ee58def8af14c67953e2b61b6b2722c4b5
Fabian <Fabian@Fabian-Laptop.fritz.box>
2024-06-16 14:07:23 +0200

    Update Wardmanagement

--- src/Stationsverwaltung/Wardmanagementservice.java
+++ src/Stationsverwaltung/Wardmanagementservice.java
@@ -13,4 +13,28 @@ public class Wardmanagementservice implements Repository {
 		return null;
 	}
 
+	@Override
+	public int WardID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int RoomID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int BedID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int StaffID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
