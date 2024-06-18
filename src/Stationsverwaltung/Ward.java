dee6d12a05dc1ac1510f5d164762dfe4e727b374
Fabian <Fabian@134.103.202.58>
2024-06-18 12:28:54 +0200

    Update Ward

--- src/Stationsverwaltung/Ward.java
+++ src/Stationsverwaltung/Ward.java
@@ -1,8 +1,8 @@
 package Stationsverwaltung;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public record Ward
 	(
@@ -13,6 +13,52 @@ public record Ward
 	)
 {
 
-	
-	
+	 public static sealed interface Command permits Create, Update, Delete  {}
+
+	  public static record Create
+	  (
+		Id<Ward> id,
+		String name,
+	    Reference<Room> room, 
+	    Reference<Staff> staff
+	  )
+	  implements Command {}
+	  
+	  public static record Update
+	  (
+		Id<Ward> id,
+		String name,
+		Reference<Room> room, 
+		Reference<Staff> staff
+	  )
+	  implements Command {}
+
+
+	  public static record Delete
+	  (
+		Id<Ward> id,
+		String name,
+		Reference<Room> room, 
+		Reference<Staff> staff
+	  )
+	  implements Command {}
+
+
+	  public static record Filter
+	  (
+	    Optional<Set<Ward>> name
+	  )
+	  {}
+
+
+	  public static interface Operations
+	  {
+	    Ward process(Command cmd) throws Exception;
+
+	    List<Stationsverwaltung.Ward> getWards(Filter filter);
+
+	    Optional<Stationsverwaltung.Ward> getWard(Id<Stationsverwaltung.Ward> id);
+	  }
+
+
 }
