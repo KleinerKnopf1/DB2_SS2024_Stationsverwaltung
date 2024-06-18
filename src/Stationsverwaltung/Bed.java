aeeacff54fef650b1251c506d7275c948f884377
Fabian <Fabian@134.103.202.58>
2024-06-18 12:59:42 +0200

    Update Bed

--- src/Stationsverwaltung/Bed.java
+++ src/Stationsverwaltung/Bed.java
@@ -1,13 +1,68 @@
 package Stationsverwaltung;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public  record Bed (
-		Id<Ward> id,
+		Id<Bed> id,
 		String name,
 		Reference<Room> room
 		)
 
 {
-	
+	public static sealed interface Command permits Create, Delete, Move, Assign, Unassign  {}
+
+	  public static record Create
+	  (
+		Id<Bed> id,
+	    Reference<Room> room
+	  )
+	  implements Command {}
+	  
+	  public static record Delete
+	  (
+		Id<Bed> id
+	  )
+	  implements Command {}
+
+
+	  public static record Move
+	  (
+		Id<Bed> id,
+		Reference<Room> room
+	  )
+	  implements Command {}
+
+	  public static record Assign
+	  (
+		Id<Bed> id,
+		Reference<Patient> patient
+	  )
+	  implements Command {}
+	  
+	  public static record Unassign
+	  (
+		Id<Bed> id
+	  )
+	  implements Command {}
+
+	  public static record Filter
+	  (
+	    Optional<Reference<Room>> room,
+	    Optional<Reference<Ward>> ward,
+	    Set<Boolean> assigned
+	  )
+	  {}
+
+
+	  public static interface Operations
+	  {
+	    Bed process(Command cmd) throws Exception;
+
+	    List<Stationsverwaltung.Bed> getBeds(Filter filter);
+
+	    Optional<Stationsverwaltung.Bed> getBed(Id<Stationsverwaltung.Bed> id);
+	  }
 
 }
